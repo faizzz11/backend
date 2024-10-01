60d4218e5f73d3835845caa75e2aae1366b7274c
faizzz11 <faizmoulavi54@gmail.com>
2024-10-01 20:26:02 +0530

    Add files via upload

---
+++ 5.6 Secrets Project/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import axios from "axios";
+
+const app=express();
+ const port =3000;
+
+app.use(express.static("public"));
+
+app.get("/", async(req,res)=>{
+try{
+   const result= await axios.get("https://secrets-api.appbrewery.com/random");
+    res.render("index.ejs",
+    {user:result.data.username,
+    secret:result.data.secret});
+} catch(error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+  
